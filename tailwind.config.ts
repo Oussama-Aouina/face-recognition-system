1c8936073e5bae1aa1b706a5c571770e7edf1fc9
Oussama Aouina <oussamaaouina99@gmail.com>
2024-07-14 12:11:02 +0100

    sertting up prettier to clarify the code

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "#072B5B",
-        activeElement: "#2EB1EE",
-        activeButton: "#F57025",
-      },
-      backgroundImage: {},
+    content: [
+        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        extend: {
+            colors: {
+                background: '#072B5B',
+                activeElement: '#2EB1EE',
+                activeButton: '#F57025',
+                bgDropzone: '#3C5372',
+                borderDropzone: '#646464',
+                secondaryText: '#E7DCDC',
+                resultText: '#C4C4C4',
+                statColor: '#7D6AEE',
+            },
+            backgroundImage: {},
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
